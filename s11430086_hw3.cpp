448fff1fb39ddf94e71b1cdf9c7ffc2f181199e4
s11430086 <f0978668301@gmail.com>
2025-11-03 23:35:09 +0800

    change1

--- s11430086_hw3.cpp
+++ s11430086_hw3.cpp
@@ -1,7 +1,14 @@
+//11430086王泳翔
 #include<iostream>
 #include<string>
-#include<cmath>
 using namespace std;
 int main(){
-    
+    string str;
+    double num1, num2;
+    cout << "輸入使用者名稱:";
+    getline(cin, str);
+    cin.ignore();
+    cout << "輸入兩個浮點數:";
+    cin >> num1 >> num2;
+    cout << "input 1:\n" << str;
 }
