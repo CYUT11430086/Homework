b67f722a084ad01399d7e66818c3f74b3102e9cf
s11430086 <f0978668301@gmail.com>
2025-11-03 23:56:09 +0800

    更新2

--- s11430086_hw3.cpp
+++ s11430086_hw3.cpp
@@ -4,11 +4,19 @@
 using namespace std;
 int main(){
     string str;
-    double num1, num2;
+    double num1, num2, add, sub, mul, div, remainder;
     cout << "輸入使用者名稱:";
     getline(cin, str);
-    cin.ignore();
+    //cin.ignore();
     cout << "輸入兩個浮點數:";
     cin >> num1 >> num2;
-    cout << "input 1:\n" << str;
+    cout << "input 1:" << str << endl;
+    add = num1 + num2;
+    sub = num1 - num2;
+    mul = num1 * num2;
+    div = num1 / num2;
+    int R1 = static_cast<int>(num1);
+    int R2 = static_cast<int>(num2);
+    remainder = R1 % R2;
+    
 }
